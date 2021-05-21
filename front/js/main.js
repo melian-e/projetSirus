2ed32a0e8c02eb116a0069afdc384f0435fde82c
Loïc GÉNEAU DE LAMARLIÈRE <loic.geneau-de-lamarliere@student.yncrea.fr>
2021-05-21 14:45:13 +0200

    affichage des 3 maps avec des toggles + selection des régions sur toutes

--- front/js/main.js
+++ front/js/main.js
@@ -12,9 +12,9 @@ socket.on('jsonaction', json =>{                            //reception JSON
     
 })
 
-document.getElementById("filtres").innerHTML = document.getElementById("filtres").innerHTML + "<div style='margin-bottom:2%;'>Map 1<input type='checkbox' id='map1' onclick='checkfiltres(1)' class='apple-switch' style='position:relative; float:right; margin-right:2%;'></div></br>"                      //affichage des différent filtres
-document.getElementById("filtres").innerHTML = document.getElementById("filtres").innerHTML + "<div style='margin-bottom:2%;'>Map 2<input type='checkbox' id='map2' onclick='checkfiltres(2)' class='apple-switch' style='position:relative; float:right; margin-right:2%;'></div></br>" 
-document.getElementById("filtres").innerHTML = document.getElementById("filtres").innerHTML + "<div style='margin-bottom:2%;'>Map 3<input type='checkbox' id='map3' onclick='checkfiltres(3)' class='apple-switch' style='position:relative; float:right; margin-right:2%;'></div></br>" 
+document.getElementById("filtres").innerHTML = document.getElementById("filtres").innerHTML + "<div style='margin-bottom:2%;'>Map des régions<input type='checkbox' id='map1' onclick='checkfiltres(1)' class='apple-switch' style='position:relative; float:right; margin-right:2%;' checked disabled></div></br>"                      //affichage des différent filtres
+document.getElementById("filtres").innerHTML = document.getElementById("filtres").innerHTML + "<div style='margin-bottom:2%;'>Map neutre<input type='checkbox' id='map2' onclick='checkfiltres(2)' class='apple-switch' style='position:relative; float:right; margin-right:2%;'></div></br>" 
+document.getElementById("filtres").innerHTML = document.getElementById("filtres").innerHTML + "<div style='margin-bottom:2%;'>Map vue satellite<input type='checkbox' id='map3' onclick='checkfiltres(3)' class='apple-switch' style='position:relative; float:right; margin-right:2%;'></div></br>" 
 
 function supression(id){                                    //fait apparaitre ou disparaitre le resume d'une action
     if(window.getComputedStyle(document.getElementById(id), null).getPropertyValue("display") == "none"){
@@ -38,24 +38,34 @@ function checkfiltres(id){                          //regarde si un autre filtre
     let id2
     let id3
 
+    document.getElementById('map'+id).setAttribute("disabled", "")
+    let canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
     if(id == 1){
         id2 = 2
         id3 = 3
+        ascko.display(imgRegions);
     }
     else if(id == 2){
         id2 = 1
         id3 = 3
+        ascko.display(imgPropa);
     }
     else{
         id2 = 1
         id3 = 2
+        ascko.display(imgSatellite);
     }
 
     if(document.getElementById('map'+id).checked == true){
         document.getElementById('map'+id2).checked = false
         document.getElementById('map'+id3).checked = false
-        alert("test")
+        alert("test");
+
+        document.getElementById('map'+id2).removeAttribute("disabled", "")
+        document.getElementById('map'+id3).removeAttribute("disabled", "")
     }
+
 
 }
 
